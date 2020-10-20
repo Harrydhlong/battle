d7fe788dbad0ad05426daec84832f2510fd34cef
Harry Long <HarryLong@Harrys-MacBook-Pro.local>
2020-10-20 15:07:36 +0100

    refactoring broke sign_in_and_play - click_button 'Submit' feature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -106,7 +106,9 @@ RSpec.configure do |config|
   require 'capybara'
   require 'capybara/rspec'
   require 'rspec'
+  require 'features/web_helpers'
 
   # tell Capybara about our app class
   Capybara.app = Battle
+
 end
